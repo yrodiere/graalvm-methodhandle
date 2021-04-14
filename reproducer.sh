73d41ad4d570e8a301bc653bbebc2f5c272f9b3a
Yoann Rodière <yoann@hibernate.org>
2021-04-14 09:35:28 +0200

    Upgrade to GraalVM 21.0

--- reproducer.sh
+++ reproducer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 NATIVE_IMAGE=$(env native-image || true)
-[ -z "$NATIVE_IMAGE" ] && NATIVE_IMAGE=/home/yrodiere/tools/java/graalvm20.3/bin/native-image
+[ -z "$NATIVE_IMAGE" ] && NATIVE_IMAGE=/home/yrodiere/tools/java/graalvm21.0/bin/native-image
 
 mvn clean package
 
@@ -14,7 +14,7 @@ java -jar $JAR
 
 echo 1>&2 "==============================================="
 echo 1>&2 "native-image compilation:"
-$NATIVE_IMAGE -J-Dsun.nio.ch.maxUpdateArraySize=100 -J-Duser.language=en -J-Dfile.encoding=UTF-8 --initialize-at-build-time= -H:InitialCollectionPolicy=com.oracle.svm.core.genscavenge.CollectionPolicy\$BySpaceAndTime -H:+JNI -jar "$JAR" -H:FallbackThreshold=0 -H:+ReportExceptionStackTraces -H:-AddAllCharsets -H:EnableURLProtocols=http -H:NativeLinkerOption=-no-pie --no-server -H:-UseServiceLoaderFeature -H:+StackTrace "$NATIVE_EXEC"
+$NATIVE_IMAGE -J-Dsun.nio.ch.maxUpdateArraySize=100 -J-Duser.language=en -J-Duser.country=US -J-Dfile.encoding=UTF-8 --initialize-at-build-time= -H:InitialCollectionPolicy=com.oracle.svm.core.genscavenge.CollectionPolicy\$BySpaceAndTime -H:+JNI -H:+AllowFoldMethods -jar "$JAR" -H:FallbackThreshold=0 -H:+ReportExceptionStackTraces -H:-AddAllCharsets -H:EnableURLProtocols=http -H:NativeLinkerOption=-no-pie --no-server -H:-UseServiceLoaderFeature -H:+StackTrace "$NATIVE_EXEC"
 
 echo 1>&2 "==============================================="
 echo 1>&2 "native-image test:"
